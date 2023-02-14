1b6c9011e81c05bd6c2b1cfe097a15bed7161cd6
Palina Nalbandian <palina.nalbandian@cleverlance.com>
2023-02-14 17:08:59 +0100

    New file

---
+++ beowling.js
@@ -0,0 +1,82 @@
+var assert = require('assert');
+
+    describe('Mathematical operations', function() {
+
+        var a = 10;
+        var b = 12;
+
+        it('4 test', function() {        
+            var c = a-b;
+            assert.equal(c, -2);
+    
+        });
+    
+
+    //Addition
+  
+    //Subtraction
+    it('Subtraction of two varibales', function() {        
+        var c = a-b;
+        assert.equal(c, -2);
+
+    });
+
+
+    //Multiplication
+    it('Multiplication of two varibales', function() {
+
+        var c = a*b;
+        assert.equal(c, 120);
+
+    });
+
+
+    //Division
+    it('Division of two varibales', function() {
+
+        var c = a/b;
+        assert.equal(c, 10/12);
+        assert.equal(c, 10/12);
+        assert.equal(c, 10/12);
+        assert.equal(c, 10/12);
+    });
+
+    it('Division of two varibales222222', function() {
+
+        var c = a/b;
+        assert.equal(c, 10/12);
+        assert.equal(c, 10/12);
+        assert.equal(c, 10/12);
+        assert.equal(c, 10/12);
+    });
+
+    it('Division of two 33333', function() {
+
+        var c = a/b;
+      
+    });
+
+    it('Lisa test 3 new', function() {
+
+        var c = a/b;
+      
+    });
+
+    it('Test on second branch', function() {
+
+        var c = a/b;
+      
+    });
+
+    it('LISA TEST ', function() {
+
+        var c = a/b;
+      
+    });
+
+
+
+
+})
+
+
