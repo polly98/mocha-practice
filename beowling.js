400e4c891422fea8976676e8c99ae6652bc68bd0
Palina Nalbandian <palina.nalbandian@cleverlance.com>
2023-02-15 10:00:44 +0100

    Jessica commit

---
+++ beowling.js
@@ -0,0 +1,19 @@
+var assert = require('assert');
+
+    describe('Mathematical operations', function() {
+
+        var a = 10;
+        var b = 12;
+
+        it('Jessica test 10', function() {        
+            var c = a-b;
+            assert.equal(c, -2);
+    
+        });
+    
+
+
+
+})
+
+
