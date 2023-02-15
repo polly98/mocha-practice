1c5ef4c90ddabcf1f4c89d790a8377f31aa24aaa
Palina Nalbandian <palina.nalbandian@cleverlance.com>
2023-02-15 10:02:20 +0100

    Lisa branch commit

---
+++ beowling.js
@@ -0,0 +1,24 @@
+var assert = require('assert');
+
+    describe('Mathematical operations', function() {
+
+        var a = 10;
+        var b = 12;
+
+        it('Jessica test 10', function() {        
+            var c = a-b;
+            assert.equal(c, -2);
+    
+        });
+        it('Lisa 1', function() {        
+            var c = a-b;
+            assert.equal(c, -2);
+    
+        });
+    
+
+
+
+})
+
+
